a4dfa384d9cbbe157dbcd43fd3e63cdfa5a003e5
Themmythorpe <soge.tope@gmail.com>
2023-09-23 18:34:19 +0100

    configuration

--- config.php
+++ config.php
@@ -6,12 +6,12 @@ define('APPLICATION', 'Catalog');
 define('HTTP_SERVER', 'http://yepcart.test/');
 
 // DIR
-define('DIR_OPENCART', getenv('DIR_OPENCART'));
+define('DIR_OPENCART', '/Users/temz/Sites/yepcart/');
 define('DIR_APPLICATION', DIR_OPENCART . 'catalog/');
 define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
 define('DIR_IMAGE', DIR_OPENCART . 'image/');
 define('DIR_SYSTEM', DIR_OPENCART . 'system/');
-define('DIR_STORAGE', getenv('DIR_STORAGE'));
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -24,8 +24,8 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', getenv('DB_USERNAME'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_DATABASE', getenv('DB_DATABASE'));
-define('DB_PORT', getenv('DB_PORT'));
-define('DB_PREFIX', getenv('DB_PREFIX'));
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '0000');
+define('DB_DATABASE', 'open_cart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'yep_');
