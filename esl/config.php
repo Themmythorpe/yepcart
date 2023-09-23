ba572b1f255c4cae4e37ae8d23eade4867c0dfbb
Themmythorpe <soge.tope@gmail.com>
2023-09-23 09:55:00 +0100

    admin config updates and storaage

--- esl/config.php
+++ esl/config.php
@@ -7,13 +7,13 @@ define('HTTP_SERVER', 'http://yepcart.test/esl/');
 define('HTTP_CATALOG', 'http://yepcart.test/');
 
 // DIR
-define('DIR_OPENCART', '/Users/temz/Sites/opencart/');
+define('DIR_OPENCART', getenv('DIR_OPENCART'));
 define('DIR_APPLICATION', DIR_OPENCART . 'esl/');
 define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
 define('DIR_IMAGE', DIR_OPENCART . 'image/');
 define('DIR_SYSTEM', DIR_OPENCART . 'system/');
 define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
-define('DIR_STORAGE', '/Users/temz/Sites/storage/');
+define('DIR_STORAGE', getenv('DIR_STORAGE'));
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -26,11 +26,11 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '0000');
-define('DB_DATABASE', 'open_cart');
-define('DB_PORT', '3306');
-define('DB_PREFIX', 'oc_');
+define('DB_USERNAME', getenv('DB_USERNAME'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_DATABASE', getenv('DB_DATABASE'));
+define('DB_PORT', getenv('DB_PORT'));
+define('DB_PREFIX', getenv('DB_PREFIX'));
 
 // OpenCart API
 define('OPENCART_SERVER', 'https://www.opencart.com/');
